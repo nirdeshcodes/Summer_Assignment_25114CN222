9bdbaa6b2d7ade742319c3437c4bca1bb10b2d6f
nirdeshcodes <nirdeshyadav713@gmail.com>
2026-06-04 22:29:55 +0530

    error solved

--- Day-1/multiplication_table.c
+++ Day-1/multiplication_table.c
@@ -3,7 +3,7 @@ int main(){
   int n,output;
   printf("Enter a number:");
   scanf("%d",&n);
-  for(int i=1;i<=n;i++){
+  for(int i=1;i<=10;i++){
     output=n*i;
   printf("%d x %d =%d\n",n,i,output);
 
