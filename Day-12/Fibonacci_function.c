163ec02fcb4ef64c8d850519d571c605f8a7b635
nirdeshcodes <nirdeshyadav713@gmail.com>
2026-06-20 23:57:54 +0530

    day 12 done

---
+++ Day-12/Fibonacci_function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void fibonacci(int n);
+
+void fibonacci(int n)
+{
+  int a = 0, b = 1, c, i;
+  for (i = 1; i <= n; i++)
+  {
+    printf("%d", a);
+    c = a + b;
+    a = b;
+    b = c;
+  }
+}
+int main()
+{
+  int n;
+  printf("Enter number of term:");
+  scanf("%d", &n);
+  fibonacci(n);
+  return 0;
+}
